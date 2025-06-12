9be513f03bcf6fe6961fb6defff17260679f2c44
Quang Le <quanglm>
2025-06-12 16:05:58 +0700

    finish login

--- src/components/ui/input/text-input.tsx
+++ src/components/ui/input/text-input.tsx
@@ -1,120 +1,119 @@
-import type { FC, ReactNode } from "react";
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-import { EyeCloseIcon, EyeIcon } from "../../../icons";
+import type { FC, ReactNode } from 'react';
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { EyeCloseIcon, EyeIcon } from '../../../icons';
 
 interface TextInputProps {
-  htmlFor?: string;
-  children: ReactNode;
-  type?: "text" | "number" | "email" | "password" | "date" | "time" | string;
-  id?: string;
-  name?: string;
-  placeholder?: string;
-  value?: string | number;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  className?: string;
-  min?: string;
-  max?: string;
-  step?: number;
-  disabled?: boolean;
-  success?: boolean;
-  error?: boolean;
-  hint?: string;
-  showPassword?: boolean;
-  toggleEyePassword?: () => void;
-  showEye?: boolean
+	htmlFor?: string;
+	children: ReactNode;
+	type?: 'text' | 'number' | 'email' | 'password' | 'date' | 'time' | string;
+	id?: string;
+	name?: string;
+	placeholder?: string;
+	value?: string | number;
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	className?: string;
+	min?: string;
+	max?: string;
+	step?: number;
+	disabled?: boolean;
+	success?: boolean;
+	error?: boolean;
+	hint?: string;
+	showPassword?: boolean;
+	toggleEyePassword?: () => void;
+	showEye?: boolean;
 }
 
-const TextInput: FC<TextInputProps> = ({ 
-  htmlFor, 
-  children, 
-  type = "text",
-  id,
-  name,
-  placeholder,
-  value,
-  onChange,
-  className = "",
-  min,
-  max,
-  step,
-  disabled = false,
-  success = false,
-  error = false,
-  hint,
-  showPassword = false,
-  showEye = false,
-  toggleEyePassword
+const TextInput: FC<TextInputProps> = ({
+	htmlFor,
+	children,
+	type = 'text',
+	id,
+	name,
+	placeholder,
+	value,
+	onChange,
+	className = '',
+	min,
+	max,
+	step,
+	disabled = false,
+	success = false,
+	error = false,
+	hint,
+	showPassword = false,
+	showEye = false,
+	toggleEyePassword,
 }) => {
-  let inputClasses = ` h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3  dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 ${className}`;
+	let inputClasses = ` h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3  dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 ${className}`;
 
-  if (disabled) {
-    inputClasses += ` text-gray-500 border-gray-300 opacity-40 bg-gray-100 cursor-not-allowed dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 opacity-40`;
-  } else if (error) {
-    inputClasses += `  border-error-500 focus:border-error-300 focus:ring-error-500/20 dark:text-error-400 dark:border-error-500 dark:focus:border-error-800`;
-  } else if (success) {
-    inputClasses += `  border-success-500 focus:border-success-300 focus:ring-success-500/20 dark:text-success-400 dark:border-success-500 dark:focus:border-success-800`;
-  } else {
-    inputClasses += ` bg-transparent text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-brand-500/20 dark:border-gray-700 dark:text-white/90  dark:focus:border-brand-800`;
-  }
+	if (disabled) {
+		inputClasses += ` text-gray-500 border-gray-300 opacity-40 bg-gray-100 cursor-not-allowed dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 opacity-40`;
+	} else if (error) {
+		inputClasses += `  border-error-500 focus:border-error-300 focus:ring-error-500/20 dark:text-error-400 dark:border-error-500 dark:focus:border-error-800`;
+	} else if (success) {
+		inputClasses += `  border-success-500 focus:border-success-300 focus:ring-success-500/20 dark:text-success-400 dark:border-success-500 dark:focus:border-success-800`;
+	} else {
+		inputClasses += ` bg-transparent text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-brand-500/20 dark:border-gray-700 dark:text-white/90  dark:focus:border-brand-800`;
+	}
 
-  return (
-    <>
-      <label
-        htmlFor={htmlFor}
-        className={clsx(
-          twMerge(
-            "mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400",
-            className,
-          ),
-        )}
-      >
-        {children}
-      </label>
-      <div className="relative w-full">
-        <input
-          type={type}
-          id={id}
-          name={name}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          min={min}
-          max={max}
-          step={step}
-          disabled={disabled}
-          className={inputClasses}
-        />
+	return (
+		<>
+			<label
+				htmlFor={htmlFor}
+				className={clsx(
+					twMerge(
+						'mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400',
+						className
+					)
+				)}
+			>
+				{children}
+			</label>
+			<div className="relative w-full">
+				<input
+					type={type}
+					id={id}
+					name={name}
+					placeholder={placeholder}
+					value={value}
+					onChange={onChange}
+					min={min}
+					max={max}
+					step={step}
+					disabled={disabled}
+					className={inputClasses}
+				/>
 
-        {hint && (
-          <p
-            className={`mt-1.5 text-xs ${
-              error
-                ? "text-error-500"
-                : success
-                ? "text-success-500"
-                : "text-gray-500"
-            }`}
-          >
-            {hint}
-          </p>
-        )}
-        {showEye && (
-          <span
-            onClick={toggleEyePassword}
-            className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
-          >
-            {showPassword ? (
-              <EyeIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
-            ) : (
-              <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
-            )}
-          </span>
-        )}
-      </div>
-      
-    </>
-  );
+				{hint && (
+					<p
+						className={`mt-1.5 text-xs ${
+							error
+								? 'text-error-500'
+								: success
+								? 'text-success-500'
+								: 'text-gray-500'
+						}`}
+					>
+						{hint}
+					</p>
+				)}
+				{showEye && (
+					<span
+						onClick={toggleEyePassword}
+						className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
+					>
+						{showPassword ? (
+							<EyeIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+						) : (
+							<EyeCloseIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+						)}
+					</span>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default TextInput;
--- src/contexts/theme-context.tsx
+++ src/contexts/theme-context.tsx
@@ -1,55 +1,60 @@
 import React from 'react';
 import { THEME_MODE, THEME_STORAGE } from '../configs';
 
-type Theme = "light" | "dark";
+type Theme = 'light' | 'dark';
 
 interface ThemeContextProps {
-  theme: Theme;
-  handleChangeTheme: () => void;
+	theme: Theme;
+	handleChangeTheme: () => void;
 }
 
 const ThemeContext = React.createContext<ThemeContextProps>({
-  theme: "light",
-  handleChangeTheme: () => {}
+	theme: 'light',
+	handleChangeTheme: () => {},
 });
 
 export const ThemeProvider = ({ children }: React.PropsWithChildren) => {
-  const [theme, setTheme] = React.useState<Theme>(THEME_MODE.LIGHT as Theme);
-  const [isInitialized, setIsInitialized] = React.useState(false);
+	const [theme, setTheme] = React.useState<Theme>(THEME_MODE.LIGHT as Theme);
+	const [isInitialized, setIsInitialized] = React.useState(false);
 
-  React.useEffect(() => {
-    const savedTheme = window.localStorage.getItem(THEME_MODE.DARK);
-    const initializeTheme = (savedTheme || THEME_MODE.LIGHT) as Theme;
+	React.useEffect(() => {
+		const savedTheme = window.localStorage.getItem(THEME_MODE.DARK);
+		const initializeTheme = (savedTheme || THEME_MODE.LIGHT) as Theme;
 
-    setTheme(initializeTheme);
-    setIsInitialized(true);
-  }, []);
+		setTheme(initializeTheme);
+		setIsInitialized(true);
+	}, []);
 
-  React.useEffect(() => {
-    if (isInitialized) {
-      window.localStorage.setItem(THEME_STORAGE, theme);
-      if (theme === THEME_MODE.DARK) {
-        document.documentElement.classList.add(THEME_MODE.DARK)
-      } else {
-        document.documentElement.classList.remove(THEME_MODE.DARK)
-      }
-    } 
-  }, [theme, isInitialized]);
+	React.useEffect(() => {
+		if (isInitialized) {
+			window.localStorage.setItem(THEME_STORAGE, theme);
+			if (theme === THEME_MODE.DARK) {
+				document.documentElement.classList.add(THEME_MODE.DARK);
+			} else {
+				document.documentElement.classList.remove(THEME_MODE.DARK);
+			}
+		}
+	}, [theme, isInitialized]);
 
-  function handleChangeTheme() {
-    setTheme(prevState => prevState === THEME_MODE.LIGHT ? THEME_MODE.DARK : THEME_MODE.LIGHT);
-  }
-  
-  return (
-    <ThemeContext.Provider
-      value={{
-        theme,
-        handleChangeTheme
-      }}
-    >
-      {children}
-    </ThemeContext.Provider>
-  )
+	function handleChangeTheme() {
+		setTheme(
+			(prevState) =>
+				(prevState === THEME_MODE.LIGHT
+					? THEME_MODE.DARK
+					: THEME_MODE.LIGHT) as Theme
+		);
+	}
+
+	return (
+		<ThemeContext.Provider
+			value={{
+				theme,
+				handleChangeTheme,
+			}}
+		>
+			{children}
+		</ThemeContext.Provider>
+	);
 };
 
 export const useThemeContext = () => React.useContext(ThemeContext);
